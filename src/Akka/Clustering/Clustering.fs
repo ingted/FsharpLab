2bd7ccca2a92d0e72dbacc672973f0aaee91496b
Dominik Kotecki <dominikus1910@gmail.com>
2018-03-24 21:36:41 +0100

    clustering

--- src/Akka/Clustering/Clustering.fs
+++ src/Akka/Clustering/Clustering.fs
@@ -4,46 +4,52 @@ open Akkling
 open Akkling.Cluster.Sharding
 open System.Configuration
 open Akka.Cluster.Tools.Singleton
-open Akkling.Cluster.Sharding
-open Akka.Actor
-open Akkling.Cluster.Sharding
 
-type ShardEnvelope = { EntityId: string; Payload: obj }
-
-type MessageExtractor<'Envelope, 'Message>(maximumShardQuantity: int) =
-    inherit HashCodeMessageExtractor(maximumShardQuantity)
-
-    override this.EntityId(message) =   
-        let envelope = (message :?> ShardEnvelope)
-        envelope.EntityId
-    
-    override this.EntityMessage(message) =
-        let envelope = (message :?> ShardEnvelope)
-        envelope.Payload    
-
-let entityFactoryFor (system: ActorSystem) (name: string) (props: Props<'Message>) : EntityFac<'Message> =
-    let clusterSharding = ClusterSharding.Get(system)
-    let adjustedProps = props
-    let shardRegion = clusterSharding.Start(name, adjustedProps.ToProps(), ClusterShardingSettings.Create(system), new MessageExtractor<_,_>(10))
-    { ShardRegion = shardRegion; TypeName = name }
-            
+type Msg =
+    | Inc of int
+    | Dec of int
+    | GetState
 
 [<EntryPoint>]
 let main argv =
     let behavior (ctx : Actor<_>) msg = printfn "%A received %s" (ctx.Self.Path.ToStringWithAddress()) msg |> ignored
 
+
+    let actor (ctx: Actor<Msg>) =
+        let rec loop(state) = actor {
+            let! msg = ctx.Receive()
+            match msg with
+            | Inc num ->
+                return loop(state + num)
+            | Dec num -> 
+                return loop(state - num)
+            | GetState ->
+                ctx.Sender() <! state
+                return loop(state)
+        }
+        loop(0)
+
     // spawn two separate systems with shard regions on each of them
 
     let system = System.create "test" (Configuration.load().WithFallback(ClusterSingletonManager.DefaultConfig()))
-    let fac1 = entityFactoryFor system "printer" <| props (actorOf2 behavior)
+    let fac1 = entityFactoryFor system "printer" <| props (actor)
 
     // wait a while before starting a second system
     System.Threading.Thread.Sleep 5000
 
-    for x in [1..100] do
-        fac1.ShardRegion.Tell({ EntityId = sprintf "john-%d" x; Payload = "Hello" })
+    let entity1 = fac1.RefFor "shard-1" "entity-1"
+    let john = fac1.RefFor "shard-1" "john"
+    let alice = fac1.RefFor "shard-2" "alice"
+    let frank = fac1.RefFor "shard-2" "frank"
 
+    entity1 <! Inc(1)
+    entity1 <! Inc(2)
+    john <! Dec(1)
+    alice <! Inc(312)
+    frank <! Inc(2311)
 
+    let frankState: int = frank <? GetState |> Async.RunSynchronously
+    printfn "State: %d" frankState
     // check which shards have been build on the second shard region
 
     System.Threading.Thread.Sleep(5000)
