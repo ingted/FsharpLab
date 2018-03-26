b6f1660ff1de2bc89418d8852706410ce151c7e5
Dominik Kotecki <dominikus1910@gmail.com>
2018-03-26 18:17:44 +0200

    small chanegs

--- src/Akka/Clustering/Clustering.fs
+++ src/Akka/Clustering/Clustering.fs
@@ -4,6 +4,44 @@ open Akkling
 open Akkling.Cluster.Sharding
 open System.Configuration
 open Akka.Cluster.Tools.Singleton
+open Akkling
+
+
+let config port = 
+    sprintf """
+            akka {
+              actor {
+                provider = cluster
+                debug {
+                    receive = on
+                    autoreceive = on
+                    lifecycle = on
+                    event-stream = on
+                    unhandled = on
+                  }
+                  serializers {
+                    hyperion = "Akka.Serialization.HyperionSerializer, Akka.Serialization.Hyperion"
+                  }
+                  serialization-bindings {
+                    "System.Object" = hyperion
+                  }
+                }
+                remote {
+                  helios.tcp {
+                    public-hostname = "localhost"
+                    hostname = "localhost"
+                    port = "%d"
+                  }
+                }
+                cluster {
+                    auto-down-unreachable-after = 5s
+                    seed-nodes = [ "akka.tcp://test@localhost:5000" ]
+                    roles = []
+                }
+                loglevel = INFO
+                stdout-loglevel = INFO
+            }""" port
+
 
 type Msg =
     | Inc of int
@@ -31,16 +69,19 @@ let main argv =
 
     // spawn two separate systems with shard regions on each of them
 
-    let system = System.create "test" (Configuration.load().WithFallback(ClusterSingletonManager.DefaultConfig()))
+    let system = System.create "test" (Configuration.parse(config(5000)).WithFallback(ClusterSingletonManager.DefaultConfig()))
     let fac1 = entityFactoryFor system "printer" <| props (actor)
 
+
+    let system = System.create "test" (Configuration.parse(config(5001)).WithFallback(ClusterSingletonManager.DefaultConfig()))
+    let fac2 = entityFactoryFor system "printer" <| props (actor)
     // wait a while before starting a second system
     System.Threading.Thread.Sleep 5000
 
     let entity1 = fac1.RefFor "shard-1" "entity-1"
-    let john = fac1.RefFor "shard-1" "john"
+    let john = fac2.RefFor "shard-1" "john"
     let alice = fac1.RefFor "shard-2" "alice"
-    let frank = fac1.RefFor "shard-2" "frank"
+    let frank = fac2.RefFor "shard-2" "frank"
 
     entity1 <! Inc(1)
     entity1 <! Inc(2)
@@ -73,5 +114,10 @@ let main argv =
     printShards fac1.ShardRegion
     printfn "Shard entities"
     printAllEntities fac1.ShardRegion
+
+    printfn "Shards active on node 'localhost:5001':"
+    printShards fac2.ShardRegion
+    printfn "Shard entities"
+    printAllEntities fac2.ShardRegion
     printfn "%A" argv
     0 // return an integer exit code
