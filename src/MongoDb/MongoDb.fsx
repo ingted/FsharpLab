23d8794846311ac36ac65d6ff9cfdbb34b4eda3a
Dominik Kotecki <dominikus1910@gmail.com>
2018-07-04 23:12:43 +0200

    small changes

--- src/MongoDb/MongoDb.fsx
+++ src/MongoDb/MongoDb.fsx
@@ -8,37 +8,38 @@
 // run the following in F# Interactive. You can ignore the project
 // (running it doesn't do anything, it just contains this script)
 #r "../../packages/DnsClient/lib/net45/DnsClient.dll"
+open Bogus
 #r "../../packages/MongoDB.Bson/lib/net45/MongoDB.Bson.dll"
 #r "../../packages/MongoDB.Driver.Core/lib/net45/MongoDB.Driver.Core.dll"
 #r "../../packages/MongoDB.Driver/lib/net45/MongoDB.Driver.dll"
-#load "..\..\packages/FsLab/FsLab.fsx"
+#r "../../packages/Bogus/lib/net40/Bogus.dll"
+#r "../../packages/NodaTime/lib/net45/NodaTime.dll"
+
 open MongoDB.Driver
 open System
-open Deedle
 open FSharp.Data
-open XPlot.GoogleCharts
-open XPlot.GoogleCharts.Deedle
 open MongoDB.Bson.Serialization.Attributes
+open MongoDB.Bson
+open NodaTime.Extensions
+open NodaTime
 
-
-// Some composite primary key
-type Id = { Id: int; Date: DateTime }
-
-type Person(id: int, day: DateTime, name: string, secondName: string option) =
-   new () = Person(0, DateTime.Today, "", None)
-    
-   [<BsonId>] member val Id: Id = { Id = id; Date = day } with get, set
-   [<BsonElement>] member val Name: string = name with get,set
-   [<BsonElement>] member val SecondName: string option = secondName with get, set
-
-   override this.ToString() =
-    sprintf "Id: %A, Name: %s, SecondName: %A" this.Id this.Name this.SecondName
+[<CLIMutable>]
+type Alarm = { [<BsonId>] Id: ObjectId; UserId: int; AlarmTime: int64 }
 
 let client = MongoClient("mongodb://127.0.0.1:27017")
-let db = client.GetDatabase("Test")
-let persons = db.GetCollection<Person>("persons")
+let db = client.GetDatabase("SmartAlarm")
+let alarms = db.GetCollection<Alarm>("alarms")
 
-//persons.InsertMany([| Person(0, DateTime.Today, "Dominik", Some("Dominik 2")); Person(0, DateTime.Today.AddDays(1.), "Dominik tommorow", None); |])
-let tommorow = (DateTime.Today.AddDays(1.))
-let find = persons.Find(fun p -> p.Id = { Id = 0; Date = DateTime.Today })
-find.ToList().ToArray()
+let generateFakeAlarms(q: int) =
+    let f = Faker<Alarm>()
+                .CustomInstantiator(fun f -> 
+                                { Id = ObjectId.GenerateNewId()
+                                  UserId = f.Random.Number(0, 1000) 
+                                  AlarmTime = f.Date.Future().ToEpoch()}
+                            )
+    f.Generate(q)
+let als = generateFakeAlarms(10000).ToArray()
+alarms.InsertMany(als)
+// let tommorow = (DateTime.Today.AddDays(1.))
+// let find = persons.Find(fun p -> p.Id = { Id = 0; Date = DateTime.Today })
+// find.ToList().ToArray()
